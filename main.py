5b859b94a9c9d0c8147c36dbde2069903d42faca
niices <niices@outlook.com>
2023-08-06 13:51:14 +0800

    1

--- main.py
+++ main.py
@@ -217,9 +217,9 @@ def main(_user, _passwd, min_1, max_1):
 
 # 获取时间戳
 def get_time():
-    url = 'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
+    url = 'https://f.m.suning.com/api/ct.do'
     response = requests.get(url, headers=headers).json()
-    t = response['currentTime2']
+    t = response['currentTime']
     return t
 
 
